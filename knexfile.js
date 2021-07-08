508b7701dbf3da9e78144653f4cc412b1386f68f
Matheus Zanetti <matheus.zanetti@fdte.io>
2021-07-08 12:02:35 -0300

    Add files

---
+++ knexfile.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const BASE_PATH = path.join(__dirname, 'db');
+
+module.exports = {
+
+    development: {
+        client: 'postgresql',
+        connection: {
+            database: 'my_db',
+            user: 'username',
+            password: 'password'
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            directory: path.join(BASE_PATH, 'migrations')
+        }
+    },
+    test: {
+        client: 'pg',
+        connection: {
+            host: "127.0.0.1",
+            user: "postgres",
+            password: "postgres",
+            database: "workflow"
+        },
+        migrations: {
+            directory: path.join(BASE_PATH, 'migrations')
+        }
+    }
+};
