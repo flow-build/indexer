a32d2e574a0c8dd83a65193d3cf077b90d013647
Matheus Zanetti <matheus.zanetti@fdte.io>
2021-07-21 11:00:48 -0300

    Add update and delete methods

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-const { v1:uuid } = require("uuid");
-// const { getEngine } = require('./src/engine');
-//
-// exports.testMessage = function (ctx, next) {
-//     console.log("Indexer plugin testMessage works!");
-//
-//     ctx.status = 200
-//     ctx.body = 'test works';
-//
-//     return ctx, next;
-// }
-//
-
-const {Index} = require("./src/class/IndexClass");
-const { Workflow }  = require("@fieldlink/workflow-engine/src/core/workflow/workflow");
-// const { knex } = require("knex")
-
+const {v1: uuid} = require("uuid");
 
 const knex = require('knex')({
     client: 'pg',
@@ -27,65 +11,22 @@ const knex = require('knex')({
     }
 });
 
-
-// const db = knex({
-//     client: 'pg',
-//     connection: {
-//         host: "localhost",
-//         user: "postgres",
-//         password: "postgres",
-//         database: "workflow",
-//         port: 5432
-//     }
-// })
-
-
- async function indexSave (ctx, next, engine) {
-    console.log("Indexer plugin indexSave works!");
-
-
-     const {uuidv1, entityType, entityId, processId, returnType} = ctx.request.body;
-
-
-     // engine.saveIndex = async (uuidv1, entityType, entityId, processId, returnType) => {
-     //
-     //     let indexObj = {
-     //         uuidv1: uuidv1,
-     //         entityType: entityType,
-     //         entityId: entityId,
-     //         processId: processId,
-     //         returnType: returnType
-     //     }
-     //
-     //    return await knex.insert(indexObj).into('index');
-     //
-     //     // return await this._db('index').insert(indexObj);
-     //
-     //     // return await new Index(uuidv1, entityType, entityId, processId, returnType).save();
-     //
-     //     // return await new Index(uuidv1, entityType, entityId, processId, returnType).save();
-     // };
-
+async function saveIndex(ctx, next) {
     try {
-        // const index = new Index(uuidv1, entityType, entityId, processId, returnType);
 
-        await knex.insert({
+        const {entity_type, entity_id, process_id, return_type} = ctx.request.body;
+
+         knex.insert({
             id: uuid(),
-            entity_type: entityType,
-            entity_id: entityId,
-            process_id: processId,
-            return_type: returnType,
+            entity_type: entity_type,
+            entity_id: entity_id,
+            process_id: process_id,
+            return_type: return_type,
             created_at: new Date()
         }).into('index');
 
-
-        // knex.select().table('workflow')
-
-
-        // const index  = await engine.saveIndex(uuidv1, entityType, entityId, processId, returnType);
-
         ctx.status = 201;
-        ctx.body = 'works';
+        ctx.body =  'works';
 
     } catch (err) {
         ctx.status = 400;
@@ -93,19 +34,51 @@ const knex = require('knex')({
     }
 }
 
-async function wfsave (ctx, next, engine) {
-    console.log("Indexer plugin wfsave works!");
-
-    // const engine = getEngine();
-    const {name, description, blueprint_spec} = ctx.request.body;
+async function fetchIndex(ctx, next) {
     try {
-        const workflow = await engine.saveWorkflow(name, description, blueprint_spec);
+        const { id } = ctx.request.body;
 
-        ctx.status = 201;
-        ctx.body = {
-            workflow_id: workflow.id,
-            workflow_url: `${ctx.header.host}${ctx.url}/${workflow.id}`
-        };
+        const fetch =  await knex.select().table('index').where({ id: id, deleted_at: null });
+
+        ctx.status = 200;
+        ctx.body = fetch;
+
+    } catch (err) {
+        ctx.status = 400;
+        ctx.body = {message: `Failed at ${err.message}`, error: err};
+    }
+}
+
+async function deleteIndex(ctx, next) {
+    try {
+        const { id } = ctx.request.body;
+
+        await knex.table('index')
+            .where({ id: id })
+            .update({ deleted_at: new Date() })
+
+        ctx.status = 200;
+        ctx.body = 'Deleted Index id ' + id;
+
+    } catch (err) {
+        ctx.status = 400;
+        ctx.body = {message: `Failed at ${err.message}`, error: err};
+    }
+}
+
+async function updateIndex(ctx, next) {
+    try {
+        const { id, entity_type, entity_id, process_id, return_type } = ctx.request.body;
+
+        await knex.table('index')
+            .where({ id: id })
+            .update({
+                entity_type, entity_id, process_id, return_type
+            });
+
+        ctx.status = 200;
+        ctx.body = 'Updated Index id ' + id;
+
     } catch (err) {
         ctx.status = 400;
         ctx.body = {message: `Failed at ${err.message}`, error: err};
@@ -113,10 +86,8 @@ async function wfsave (ctx, next, engine) {
 }
 
 module.exports = {
-    test: function() {
-        console.log('var is', this.myvar);
-        console.log('var is', this.myvar);
-    },
-    indexSave,
-    wfsave
+    saveIndex,
+    fetchIndex,
+    deleteIndex,
+    updateIndex
 };
