99e650aec59805f2a1b746cec7d28c5b46bb7c22
Matheus Zanetti <matheus.zanetti@fdte.io>
2021-07-22 16:36:25 -0300

    WIP - refactor

--- src/class/IndexClass.js
+++ src/class/IndexClass.js
@@ -1,70 +1,73 @@
-const {PersistedEntity} = require('@fieldlink/workflow-engine/src/core/workflow/base');
 
-class Index extends PersistedEntity {
+class Index {
 
-    static getEntityClass() {
-        return Index;
+    async testIndex() {
+        console.log ('testIndex works');
     }
 
-    static serialize(index) {
-        return {
-            uuid1: index._uuid1,
-            entityType: index._entityType,
-            entityId: index._entityId,
-            processId: index._processId,
-            returnType: index._returnType,
-            created_at: index._created_at,
-            deleted_at: index._deleted_at
-        };
-    }
-
-    static deserialize(serialized) {
-        if (serialized) {
-            const index = new Index(
-                serialized.uuid1,
-                serialized.entityType,
-                serialized.entityId,
-                serialized.processId,
-                serialized.returnType,
-                serialized.created_at,
-                serialized.deleted_at
-            );
-            return index;
-        }
-        return undefined;
-    }
-
-    constructor(uuid1, entityType, entityId, processId, returnType, created_at, deleted_at) {
-        super();
-
-        this._uuid1 = uuid1;
-        this._entityType = entityType;
-        this._entityId = entityId;
-        this._processId = processId;
-        this._returnType = returnType;
-        this._created_at = created_at;
-        this._deleted_at = deleted_at;
-    }
-
-    get uuid1() {
-        return this._uuid1;
-    }
-
-    get entityType() {
-        return this._entityType;
-    }
-
-    get entityId() {
-        return this._entityId;
-    }
-
-    get processId() {
-        return this._processId;
-    }
-
-    get returnType() {
-        return this._returnType;
-    }
+    // static getEntityClass() {
+    //     return Index;
+    // }
+    //
+    // static serialize(index) {
+    //     return {
+    //         uuid1: index._uuid1,
+    //         entityType: index._entityType,
+    //         entityId: index._entityId,
+    //         processId: index._processId,
+    //         returnType: index._returnType,
+    //         created_at: index._created_at,
+    //         deleted_at: index._deleted_at
+    //     };
+    // }
+    //
+    // static deserialize(serialized) {
+    //     if (serialized) {
+    //         const index = new Index(
+    //             serialized.uuid1,
+    //             serialized.entityType,
+    //             serialized.entityId,
+    //             serialized.processId,
+    //             serialized.returnType,
+    //             serialized.created_at,
+    //             serialized.deleted_at
+    //         );
+    //         return index;
+    //     }
+    //     return undefined;
+    // }
+    //
+    // constructor(uuid1, entityType, entityId, processId, returnType, created_at, deleted_at) {
+    //     super();
+    //
+    //     this._uuid1 = uuid1;
+    //     this._entityType = entityType;
+    //     this._entityId = entityId;
+    //     this._processId = processId;
+    //     this._returnType = returnType;
+    //     this._created_at = created_at;
+    //     this._deleted_at = deleted_at;
+    // }
+    //
+    // get uuid1() {
+    //     return this._uuid1;
+    // }
+    //
+    // get entityType() {
+    //     return this._entityType;
+    // }
+    //
+    // get entityId() {
+    //     return this._entityId;
+    // }
+    //
+    // get processId() {
+    //     return this._processId;
+    // }
+    //
+    // get returnType() {
+    //     return this._returnType;
+    // }
 
 
     /*
@@ -92,26 +95,26 @@ class Index extends PersistedEntity {
             }
 
 */
-
-            set process_state_id(process_state_id) {
-                // this._process_state_id = process_state_id;
-            }
-
-            get status() {
-                // return this._status;
-            }
-
-            set status(status) {
-                // this._status = status;
-            }
-
-            get props() {
-                // return this._props;
-            }
-
-            set props(props) {
-                // this._props = props;
-            }
+            //
+            // set process_state_id(process_state_id) {
+            //     // this._process_state_id = process_state_id;
+            // }
+            //
+            // get status() {
+            //     // return this._status;
+            // }
+            //
+            // set status(status) {
+            //     // this._status = status;
+            // }
+            //
+            // get props() {
+            //     // return this._props;
+            // }
+            //
+            // set props(props) {
+            //     // this._props = props;
+            // }
 
 
 }
