bca243d66e27cea96b3a59ad74c1ab4dc81f2a26
Gustavo Haramura <gustavo.haramura@fdte.io>
2021-08-05 19:19:08 -0300

    perf(persist): add indexes to migration

--- db/migrations/20210707192414_create_index_table.js
+++ db/migrations/20210707192414_create_index_table.js
@@ -1,17 +1,17 @@
-exports.up = function(knex) {
-    return knex.schema.createTable("index", table => {
-        table.uuid("id").primary();
-        table.string("entity_type").notNullable();
-        table.string("entity_id").notNullable();
-        table.string("process_id").notNullable();
-        table.string("return_type").notNullable();
-        table.timestamp("created_at").notNullable();
-        table.timestamp("deleted_at");
-        table.index(["process_id"],"idx_process");
-        table.index(["entity_id"],"idx_entity");
-    });
+exports.up = function (knex) {
+  return knex.schema.createTable("index", (table) => {
+    table.uuid("id").primary();
+    table.string("entity_type").notNullable();
+    table.string("entity_id").notNullable();
+    table.string("process_id").notNullable();
+    table.string("return_type").notNullable();
+    table.timestamp("created_at").notNullable();
+    table.index(["process_id"], "idx_process");
+    table.index(["entity_id"], "idx_entity");
+    table.unique(["process_id", "entity_id"]);
+  });
 };
 
-exports.down = function(knex) {
-    return knex.schema.dropTable("index");
+exports.down = function (knex) {
+  return knex.schema.dropTable("index");
 };
