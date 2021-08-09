964c8d35dfdde24d257b935d8c62ecad784a7d7e
Gustavo Haramura <gustavo.haramura@fdte.io>
2021-08-09 16:41:22 -0300

    feat(facade): adds validation to createIndex

--- src/class/IndexClass.js
+++ src/class/IndexClass.js
@@ -1,47 +1,65 @@
 const { PersistorProvider } = require("../persist/provider");
 const { IndexEntity } = require("../entities/index");
-
+const { validateDataWithSchema } = require("../validators/base");
 class Index {
-    constructor(db){
-        this._db = db;
-        PersistorProvider.getPersistor(db);
-    }
+  constructor(db) {
+    this._db = db;
+    PersistorProvider.getPersistor(db);
+  }
 
-    async testIndex() {
-        console.log ('testIndex works');
+  async createIndex(indexObj) {
+    console.log("createIndex");
+    const schema = IndexEntity.getSchema();
+    const validation = await validateDataWithSchema(schema, indexObj);
+    if (validation.isValid) {
+      const response = await new IndexEntity(indexObj).save();
+      if (response.error) {
+        return {
+          errorType: "save",
+          errorMessage: response.error,
+        };
+      } else {
+        return response;
+      }
+    } else {
+      return {
+        errorType: "validation",
+        errorMessage: validation.errors,
+      };
     }
+  }
 
-    async createIndex(indexObj) {
-        console.log ('createIndex');
-        return await new IndexEntity(indexObj).save();
-    }
+  async fetchProcessesByEntityType(entityType) {
+    console.log("fetchProcessesByEntityType");
+    return await new IndexEntity().getProcessByEntityType(entityType);
+  }
 
-    async fetchProcessesByEntityType(entityType) {
-        console.log ('fetchProcessesByEntityType');
-        return await new IndexEntity().getProcessByEntityType(entityType);
-    }
+  async fetchProcessByEntity(entityId, limit = 10) {
+    console.log("fetchProcessByEntity");
+    return await new IndexEntity().getByEntity(entityId, limit);
+  }
 
-    async fetchProcessByEntity(entityId, limit = 10) {
-        return await new IndexEntity().getByEntity(entityId, limit);
-    }
+  async fetchEntitiesByProcess(processId, limit = 10) {
+    console.log("fetchEntitiesByProcess");
+    return await new IndexEntity().getByProcess(processId, limit);
+  }
 
-    async fetchEntitiesByProcess(processId, limit = 10) {
-        return await new IndexEntity().getByProcess(processId, limit);
-    }
+  async removeIndex(id) {
+    console.log("removeIndex");
+    return await new IndexEntity().delete(id);
+  }
 
-    async removeIndex(id) {
-        return await new IndexEntity().delete(id);
-    }
+  async removeIndexByProcess(processId) {
+    console.log("removeIndexByProcess");
+    return await new IndexEntity().deleteAllByProcess(processId);
+  }
 
-    async removeIndexByProcess(processId) {
-        return await new IndexEntity().deleteAllByProcess(processId);
-    }
-
-    async removeInvexByEntity(entityId) {
-        return await new IndexEntity().deleteAllByEntity(entityId)
-    }
+  async removeIndexByEntity(entityId) {
+    console.log("removeIndexByEntity");
+    return await new IndexEntity().deleteAllByEntity(entityId);
+  }
 }
 
 module.exports = {
-    Index
+  Index,
 };
