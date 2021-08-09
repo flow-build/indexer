a555e869a6659ca9ff139abc48f568ce04c870f3
Gustavo Haramura <gustavo.haramura@fdte.io>
2021-08-09 16:40:41 -0300

    test(tests): reorg seeds

--- src/tests/indexClass.test.js
+++ src/tests/indexClass.test.js
@@ -1,30 +1,72 @@
 /* eslint-disable no-undef */
-const { db } = require("./utils/db");
+const { db, load, clean } = require("./utils/db");
 const { Index } = require("../class/IndexClass");
 const { v1: uuid } = require("uuid");
 
+beforeAll(async () => {
+  await load(db);
+});
+
 afterAll(async () => {
+  await clean(db);
   await db.destroy();
 });
 
 describe("IndexClass tests", () => {
   const entityId = uuid();
   const processId = uuid();
+  const sampleIndex = {
+    entity_type: "any",
+    entity_id: entityId,
+    process_id: processId,
+  };
 
   test("createIndex should work", async () => {
+    const _index = new Index(db);
+    const response = await _index.createIndex(sampleIndex);
+    expect(response).toBeDefined();
+    expect(response.id).toBeDefined();
+  });
+
+  test("createIndex should require processId", async () => {
+    const _index = new Index(db);
+    const idxObj = {
+      entity_type: "any",
+      entity_id: entityId,
+    };
+    const response = await _index.createIndex(idxObj);
+    expect(response).toBeDefined();
+    expect(response.id).not.toBeDefined();
+    expect(response.errorType).toBeDefined();
+    expect(response.errorType).toBe("validation");
+  });
+
+  test("createIndex should require entityId", async () => {
+    const _index = new Index(db);
+    const idxObj = {
+      entity_type: "any",
+      process_id: processId,
+    };
+    const response = await _index.createIndex(idxObj);
+    expect(response).toBeDefined();
+    expect(response.id).not.toBeDefined();
+    expect(response.errorType).toBeDefined();
+    expect(response.errorType).toBe("validation");
+  });
+
+  test("createIndex should not allow duplicated entity & process", async () => {
     const _index = new Index(db);
     const idxObj = {
       entity_type: "any",
       entity_id: entityId,
       process_id: processId,
-      return_type: "any",
     };
     const response = await _index.createIndex(idxObj);
     expect(response).toBeDefined();
-    expect(response.id).toBeDefined();
+    expect(response.id).not.toBeDefined();
+    expect(response.errorType).toBeDefined();
+    expect(response.errorType).toBe("save");
   });
-
-  test("createIndex should not allow duplicated entity & process", async () => {});
 
   test("fetchProcessesByEntityType should work", async () => {
     const _index = new Index(db);
@@ -46,19 +88,30 @@ describe("IndexClass tests", () => {
     expect(response).toHaveLength(1);
   });
 
-  test("deleteIndex should work", async () => {});
-
-  test("fetchProcessByEntity should not retrieve deleted indexes", async () => {
+  test("removeIndex should work", async () => {
+    const seededProcessId = "1022fcdf-fc00-4474-9e9a-a184b5b4d70a";
+    const seededIndexId = "17d8d0de-a9e3-4e9c-acbb-b6e9c266f0ac";
     const _index = new Index(db);
-    const response = await _index.fetchProcessByEntity(entityId);
-    expect(response).toBeDefined();
-    expect(response).toHaveLength(1);
+    const delResponse = await _index.removeIndex(seededIndexId);
+    expect(delResponse).toBeDefined();
+    const fetchResponse = await _index.fetchEntitiesByProcess(seededProcessId);
+    expect(fetchResponse).toHaveLength(0);
   });
 
-  test("fetchEntitiesByProcess should not retrieve deleted indexes", async () => {
+  test("removeIndexByProcess should work", async () => {
     const _index = new Index(db);
-    const response = await _index.fetchProcessByEntity(entityId);
-    expect(response).toBeDefined();
-    expect(response).toHaveLength(1);
+    const delResponse = await _index.removeIndexByProcess(processId);
+    expect(delResponse).toBeDefined();
+    const fetchResponse = await _index.fetchEntitiesByProcess(processId);
+    expect(fetchResponse).toHaveLength(0);
+  });
+
+  test("removeIndexByEntity should work", async () => {
+    const _index = new Index(db);
+    await _index.createIndex(sampleIndex);
+    const delResponse = await _index.removeIndexByEntity(entityId);
+    expect(delResponse).toBeDefined();
+    const fetchResponse = await _index.fetchEntitiesByProcess(processId);
+    expect(fetchResponse).toHaveLength(0);
   });
 });
--- src/tests/utils/db.js
+++ src/tests/utils/db.js
@@ -1,8 +1,19 @@
 const Knex = require("knex");
+const samples = require("./samples");
 
 const knexConfig = require("../../../knexfile")[process.env.KNEX_ENV || "test"];
 const db = Knex(knexConfig);
 
+const clean = async (_db) => {
+  await _db("index").del();
+};
+
+const load = async (_db) => {
+  await _db("index").insert(samples["index"]);
+};
+
 module.exports = {
   db,
+  clean,
+  load,
 };
