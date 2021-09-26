79b7c06228ae97381304e57c3658b41b0574f4dd
Gustavo Haramura <gustavo.haramura@fdte.io>
2021-09-26 15:53:43 -0300

    feat(facade): allow multiple fetchEntitiesByProcess

--- src/class/IndexClass.js
+++ src/class/IndexClass.js
@@ -9,13 +9,13 @@ class Index {
   }
 
   async createIndex(indexObj) {
-    logger.debug("[Indexer] createIndex");
+    logger.debug("createIndex");
     const schema = IndexEntity.getSchema();
     const validation = await validateDataWithSchema(schema, indexObj);
     if (validation.isValid) {
       const response = await new IndexEntity(indexObj).save();
       if (response.error) {
-        logger.warn("[Indexer] createIndex error on save", response.error);
+        logger.warn("createIndex error on save");
         return {
           errorType: "save",
           errorMessage: response.error,
@@ -24,7 +24,7 @@ class Index {
         return response;
       }
     } else {
-      logger.warn("[Indexer] createIndex error on validation", validation.errors);
+      logger.warn("createIndex error on validation");
       return {
         errorType: "validation",
         errorMessage: validation.errors,
@@ -33,32 +33,56 @@ class Index {
   }
 
   async fetchProcessesByEntityType(entity_type) {
-    logger.debug("[Indexer] fetchProcessesByEntityType");
+    logger.debug("fetchProcessesByEntityType");
     return await new IndexEntity().getProcessByEntityType(entity_type);
   }
 
   async fetchProcessByEntity(entity_id, limit = 10) {
-    logger.debug("[Indexer] fetchProcessByEntity");
+    logger.debug("fetchProcessByEntity");
     return await new IndexEntity().getByEntity(entity_id, limit);
   }
 
   async fetchEntitiesByProcess(process_id, limit = 10) {
-    logger.debug("[Indexer] fetchEntitiesByProcess");
-    return await new IndexEntity().getByProcess(process_id, limit);
+    logger.debug("fetchEntitiesByProcess");
+    const processIdSchema = {
+      oneOf: [
+        { type: "string", format: "uuid" },
+        { type: "array", items: { type: "string", format: "uuid" } },
+      ],
+    };
+    const validation = await validateDataWithSchema(processIdSchema, process_id);
+    if (validation.isValid) {
+      const Index = new IndexEntity();
+      if (typeof process_id === "string") {
+        logger.debug(`fetchEntitiesByProcess, single process, ${process_id}`);
+        return await Index.getByProcess(process_id, limit);
+      } else {
+        logger.debug(`fetchEntitiesByProcess, multiple process, ${process_id}`);
+        const calls = process_id.map((process) => Index.getByProcess(process, limit));
+        const response = await Promise.all(calls);
+        return response;
+      }
+    } else {
+      logger.warn("fetchProcessByEntity error on validation");
+      return {
+        errorType: "validation",
+        errorMessage: validation.errors,
+      };
+    }
   }
 
   async removeIndex(id) {
-    logger.debug("[Indexer] removeIndex");
+    logger.debug("removeIndex");
     return await new IndexEntity().delete(id);
   }
 
   async removeIndexByProcess(process_id) {
-    logger.debug("[Indexer] removeIndexByProcess");
+    logger.debug("removeIndexByProcess");
     return await new IndexEntity().deleteAllByProcess(process_id);
   }
 
   async removeIndexByEntity(entity_id) {
-    logger.debug("[Indexer] removeIndexByEntity");
+    logger.debug("removeIndexByEntity");
     return await new IndexEntity().deleteAllByEntity(entity_id);
   }
 }
