59adc654cd8d8154b0ce0c30ef350669d92fa29c
Gustavo Haramura <gustavo.haramura@fdte.io>
2021-09-26 15:54:11 -0300

    build: fix knex env for docker

--- knexfile.js
+++ knexfile.js
@@ -1,43 +1,62 @@
-const path = require('path');
-const BASE_PATH = path.join(__dirname, 'db');
+const path = require("path");
+const BASE_PATH = path.join(__dirname, "db");
 
 module.exports = {
-    test: {
-        client: 'pg',
-        connection: {
-            host: 'localhost',
-            database: "indexer",
-            user: 'username',
-            password: 'password'
-        },
-        pool: {
-            min: 2,
-            max: 10
-        },
-        migrations: {
-            directory: path.join(BASE_PATH, 'migrations')
-        },
-        seeds: {
-          directory: path.join(BASE_PATH, "seeds"),
-        },
+  docker: {
+    client: "pg",
+    connection: {
+      host: "index_db",
+      database: "indexer",
+      user: "username",
+      password: "password",
     },
-    development: {
-      client: "pg",
-      connection: {
-        host: process.env.DB_HOST,
-        database: process.env.DB_DATABASE,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-      },
-      pool: {
-        min: 2,
-        max: 10,
-      },
-      migrations: {
-        directory: path.join(BASE_PATH, "db/migrations"),
-      },
-      seeds: {
-        directory: path.join(BASE_PATH, "db/seeds"),
-      },
-    }
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: path.join(BASE_PATH, "migrations"),
+    },
+    seeds: {
+      directory: path.join(BASE_PATH, "seeds"),
+    },
+  },
+  test: {
+    client: "pg",
+    connection: {
+      host: "localhost",
+      database: "indexer",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: path.join(BASE_PATH, "migrations"),
+    },
+    seeds: {
+      directory: path.join(BASE_PATH, "seeds"),
+    },
+  },
+  development: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_DATABASE,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: path.join(BASE_PATH, "db/migrations"),
+    },
+    seeds: {
+      directory: path.join(BASE_PATH, "db/seeds"),
+    },
+  },
 };
